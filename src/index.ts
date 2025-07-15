066f3100d9d44a4cfca097f13a4f187a740925c1
Sahil Dahal <83700338+sahilverse@users.noreply.github.com>
2025-07-15 23:00:38 +0545

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import './loadEnv';
 import app from './app';
 import prisma from './config/prisma';
 import { PORT } from './constants';
-import { createServer } from 'http';
-import { initSocketIO } from './config/socket';
 
-const server = createServer(app);
 
 
 // Database connection
@@ -14,7 +11,7 @@ prisma.$connect()
         console.log('Connected to the database successfully');
 
         // Start the server
-        server.listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log(`Server is running at http://localhost:${PORT}`);
         });
     })
